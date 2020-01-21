cbd757b9436cedfd43affb4877103891b4b799b3
Boban Acimovic <boban.acimovic@gmail.com>
2020-01-21 21:56:44 +0100

    swith bg image with theme switch

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,7 +1,7 @@
 <style lang="scss">
   header {
     height: 50vh;
-    background-image: url(/bg-light.jpg);
+    background-image: var(--image-bg);
     background-size: cover;
     background-position: 0% 25%;
     clip-path: polygon(0 0, 100% 0, 100% 90%, 0 100%);
